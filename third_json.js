324b6eab3407eec69e437d6e5004132d3b34a238
QnAline <133673225+QnAline@users.noreply.github.com>
2023-11-04 08:47:56 +0200

    Add files via upload

---
+++ third_json.js
@@ -0,0 +1,14 @@
+[
+    {
+        "id":2,
+        "Mother":"Providence",
+        "Martial status":"Married"
+    },
+
+
+    {
+    "id":1,
+    "First child":"Alius",
+    "Martial status":"single"
+    }
+]
